1c19158b5200ac7e7ab71ea9b93885c47103d193
Vishakh <vishakh@yahoo.com>
2014-03-09 20:34:04 -0400

    Added support for temperature script.

---
+++ temperature.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python2.7
+# This is a dummy script to serve as an example of a temperature script (see README).
+# The script should print out only the temperature and nothing else!
+print "25"
